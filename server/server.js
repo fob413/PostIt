95fd34433ef9aa4efda8d0d2e79084a256c3d525
funsho <fob1493@gmail.com>
2017-07-06 14:44:56 +0100

    template pages for the app

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require( 'express' );
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+let users = require('./data');
 
 app.use(bodyParser.urlencoded( {
 
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded( {
 app.use(cors());
 
 app.get( '/api/users', ( request, response) => {
-  response.send('Hello World');
+  response.json(users);
 });
 
 
