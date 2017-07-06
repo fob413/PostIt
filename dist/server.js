05da78b0df35554c16b264cc95d7400407d8345e
funsho <fob1493@gmail.com>
2017-07-06 11:25:45 +0100

    Setup and test of installed packages

---
+++ dist/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var me = 'Hi there';
+console.log(me);
+
+var sum = function sum(a, b) {
+
+  console.log(a + b);
+};
+
+sum(5, 5);
---
+++ server/server.js
@@ -0,0 +1,10 @@
+let me = 'Hi there';
+console.log( me );
+
+let sum = (a, b) => {
+
+  console.log( a+b );
+
+}
+
+sum( 5, 5 );
