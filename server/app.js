def8e33e60512f9ffbf1f0510df35d9f63c2bb3b
funsho <fob1493@gmail.com>
2017-07-11 11:37:21 +0100

    Users Sign up route

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+require('./routes')(app);
 /* setup a default catch-all route that sends back a
 welcome message in json format*/
 app.get('*', (req, res) => res.status(200).send({
