5af9baf2ed89fc6c7556b96803f55b587c2dd258
funsho <fob1493@gmail.com>
2017-07-11 12:00:50 +0100

    Refactored models/index

--- dist/models/index.js
+++ dist/models/index.js
@@ -3,9 +3,13 @@
 var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
+
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env];
+
+var configUrl = require('../config/config.json');
+
+var config = configUrl[env];
 var db = {};
 
 var sequelize = void 0;
@@ -16,9 +20,9 @@ if (config.use_env_variable) {
 }
 
 fs.readdirSync(__dirname).filter(function (file) {
-  file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+  return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
 }).forEach(function (file) {
-  var model = sequelize['import'](path.join(__dirname, file));
+  var model = sequelize.import(path.join(__dirname, file));
   db[model.name] = model;
 });
 
