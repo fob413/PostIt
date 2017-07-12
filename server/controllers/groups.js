e9e44048c758b774d453d0527eb18859a36a6551
funsho <fob1493@gmail.com>
2017-07-12 17:03:43 +0100

    Groups and Members Routes

--- server/controllers/groups.js
+++ server/controllers/groups.js
@@ -1,11 +1,45 @@
 const groups = require('../models').Groups;
 const members = require('../models/').Members;
+const Users = require('../models/').Users;
 
 
 module.exports = {
 
   create(req, res) {
-    return groups
+    return Users
+    .findOne({
+      where: {
+        id: req.body.userId
+      }
+    })
+    .then((user) => {
+      if (user) {
+        if (user.isLoggedin) {
+          return groups.create({
+            GroupName: req.body.GroupName,
+            Description: req.body.Description,
+          })
+          .then((group) => {
+            members
+            .create({
+              userId: req.body.userId,
+              groupId: group.id,
+            })
+            .then(res.status(201).send(group));
+          })
+          .catch(error => res.status(400).send(error));
+        } else {
+          res.status(401).json('Not logged in');
+        }
+      } else {
+        res.status(401).json('SignUp to access this service');
+      }
+    })
+    .then(user => res.status(201).send(user))
+    .catch(error => res.status(400).send(error));
+    
+    /*
+    groups
     .create({
       GroupName: req.body.GroupName,
       Description: req.body.Description,
@@ -18,7 +52,7 @@ module.exports = {
       })
       .then(res.status(201).send(group));
     })
-    .catch(error => res.status(400).send(error));
+    .catch(error => res.status(400).send(error));*/
   },
 
   list(req, res) {
