d683b1b4cbe90d9fb70e8d4a09ab5d61aa6451a1
funsho <fob1493@gmail.com>
2017-07-13 01:22:57 +0100

    test

--- server/test/userTests.js
+++ server/test/userTests.js
@@ -4,10 +4,48 @@ const supertest = require('supertest');
 
 const api = supertest('http://localhost:8000');
 
-describe('User', () => {
+describe('Valid User', () => {
   it('should return a 200 response', (done) => {
     api.get('/api')
     .set('Accept', 'application/json')
     .expect(200, done);
+  });
+
+  it('should signup a user', (done) => {
+    api.post('/api/user/signup')
+    .set('Accept', 'application/x-www-form-urlencoded')
+    .send({
+      UserName: 'Bayo',
+      email: 'bayo@gmail.com',
+      password: '1234'
+    })
+    .expect(201)
+    .end((err, res) => {
+      console.log('<,,,,,,,,,,,,-------------------' + res.body);
+      expect(res.body.UserName).to.equal('Funsho');
+      done();
+    });
+  });
+
+  it('should not return an error signiing up a user', (done) => {
+    api.post('/api/user/signup')
+    .field('UserName', 'Bayo')
+    .field('email', 'bayo@yahoo.com')
+    .field('password', '1234')
+    .end((err, res) => {
+      expect(err).to.be.null;
+      done();
+    });
+  });
+
+  it('should signup a user', (done) => {
+    api.post('/api/user/signup')
+    .field('UserName', 'Titi')
+    .field('email', 'Titi@yahoo.com')
+    .field('password', '1234')
+    .end((err, res) => {
+      expect(res).to.have.status(201);
+      done();
+    });
   });
 });
