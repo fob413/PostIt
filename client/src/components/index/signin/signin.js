d52a0ac7cc0a49c38c4362ed0379e30f5c384059
funsho <fob1493@gmail.com>
2017-08-01 12:52:17 +0100

    Signup Signin Component

--- client/src/components/index/signin/signin.js
+++ client/src/components/index/signin/signin.js
@@ -3,8 +3,67 @@ import React from 'react';
 class Signin extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Signin Component</h1>
+      <div className="container">
+        <div className="row center-align">
+          <img width="30%" 
+            src="./../../../image/postitD.png"
+            alt="PostIt Logo"
+          />
+        </div>
+
+        <div className="row">
+          <div className="container col s12">
+            <form>
+              <div className="row">
+                <div className="input-field col s12 m12">
+                  <input
+                    className="validate"
+                    type="text"
+                    id="userName"
+                    data-error="wrong"
+                    data-success="right"
+                  />
+                  <label htmlFor="userName">
+                    Username
+                  </label>
+                </div>
+              </div>
+
+              <div className="input-field col s12 m12">
+                <input 
+                  className="validate"
+                  type="password"
+                  id="password"
+                  data-error="wrong"
+                  data-success="right"
+                />
+                <label htmlFor="password">
+                  Password
+                </label>
+              </div>
+
+              <div className="row center-align">
+                <button
+                  className="btn-large green darken-4 waves effect"
+                  type="submit"
+                  name="action"
+                >
+                  Sign In
+                </button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+        <div className="center-align">
+          <br />
+          <br />
+          <p>
+            OR
+            <br />
+            <a className="green-text text-darken-1" href="/">Sign Up</a>
+          </p>
+        </div>
       </div>
     );
   }
--- client/src/components/index/signup/signup.js
+++ client/src/components/index/signup/signup.js
@@ -11,6 +11,73 @@ class Signup extends React.Component {
             alt="PostIt Logo"
           />
         </div>
+
+        <div className="row">
+          <div className="container col s12">
+            <form>
+              <div className="row">
+                <div className="input-field col s12 m6">
+                  <input
+                    className="validate"
+                    type="text"
+                    id="userName"
+                    data-error="wrong"
+                    data-success="right"
+                  />
+                  <label htmlFor="userName">
+                    Username
+                  </label>
+                </div>
+
+                <div className="input-field col s12 m6">
+                  <input
+                    className="validate"
+                    type="email"
+                    id="email"
+                    data-error="wrong"
+                    data-success="right"
+                  />
+                  <label htmlFor="email">
+                    Email
+                  </label>
+                </div>
+              </div>
+
+              <div className="input-field col s12 m12">
+                <input 
+                  className="validate"
+                  type="password"
+                  id="password"
+                  data-error="wrong"
+                  data-success="right"
+                />
+                <label htmlFor="password">
+                  Password
+                </label>
+              </div>
+
+              <div className="row center-align">
+                <button
+                  className="btn-large green darken-4 waves effect"
+                  type="submit"
+                  name="action"
+                >
+                  Sign Up
+                </button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+        <div className="center-align">
+          <br />
+          <br />
+          <p>
+            OR
+            <br />
+            <a className="green-text text-darken-1" href="/signin">Sign In</a>
+          </p>
+        </div>
       </div>
     );
   }
