0c5ed32881b842902025d8b4ea7fd979fb3a8c9d
funsho <fob1493@gmail.com>
2017-08-01 12:28:33 +0100

    Signup component

--- client/src/components/index/signup/signup.js
+++ client/src/components/index/signup/signup.js
@@ -4,9 +4,13 @@ import {Link} from 'react-router';
 class Signup extends React.Component {
   render() {
     return (
-      <div>
-        <h1>WELCOME</h1>
-        <a href="/signin">SIGN IN</a>
+      <div className="container">
+        <div className="row center-align">
+          <img width="30%" 
+            src="./../../../image/postitD.png"
+            alt="PostIt Logo"
+          />
+        </div>
       </div>
     );
   }
