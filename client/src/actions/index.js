d1826747fbd9c8b522fa6c60c5dac5141447d0cd
funsho <fob1493@gmail.com>
2017-08-09 10:56:48 +0100

    Sign in sign up actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import C from '../constants';
+
+export const signUp = () => {
+  return {
+    type: C.SIGN_UP
+  };
+};
+
+export const signIn = () => {
+  return {
+    type: C.SIGN_IN
+  };
+};
