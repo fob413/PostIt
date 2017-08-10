a545e25c7e8982ea0dc5ca56f3b12a98383289ec
funsho <fob1493@gmail.com>
2017-08-10 13:44:54 +0100

    Refactor api with jwt

--- server/controllers/groups.js
+++ server/controllers/groups.js
@@ -1,49 +1,78 @@
 import db from '../models/index';
+import jwt from 'jsonwebtoken';
 
 const Users = db.Users;
 const Groups = db.Groups;
 const Members = db.Members;
+const secret = process.env.SECRET_TOKEN;
 
 export default {
 
   create(req, res) {
-    return Users
-    .findOne({
-      where: {
-        id: req.body.userId
-      }
-    })
-    .then((user) => {
-      if (user) {
-        if (user.isLoggedin) {
-          if (req.body.GroupName.length > 0) {
-            return Groups
-            .create({
-              GroupName: req.body.GroupName,
-              Description: req.body.Description,
-            })
-            .then((group) => {
-              Members
-              .create({
-                userId: req.body.userId,
-                groupId: group.id,
-              })
-              .then(res.status(201).send(group));
-            })
-            .catch(error => res.status(400).send(error));
-          } else {
-            res.status(400).send({
-              message: 'Input a name for the group'
-            });
-          }
+    if (req.headers['token']) {
+      const token = req.headers['token'];
+      jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+          return res.json({
+            success: false,
+            message: 'failed to authenticate token'
+          });
         } else {
-          res.status(401).json('Not logged in');
+          req.decoded = decoded;
+          return Users
+          .findOne({
+            where: {
+              UserName: req.decoded.UserName
+            }
+          })
+          .then((user) => {
+            if (user) {
+              if (user.isLoggedin) {
+                if (req.body.GroupName.length > 0) {
+                  return Groups
+                  .create({
+                    GroupName: req.body.GroupName,
+                    Description: req.body.Description
+                  })
+                  .then((group) => {
+                    Members
+                    .create({
+                      userId: user.userId,
+                      groupId: group.id
+                    })
+                    .then(res.status(201).send(group));
+                  })
+                  .catch(err => res.status(400).send({
+                    success: false,
+                    message: err.message
+                  }));
+                } else {
+                  res.status(400).send({
+                    success: false,
+                    message: 'Input a name for the group'
+                  });
+                }
+              }
+            } else {
+              res.status(401).json({
+                success: false,
+                message: 'Signup to access this service'
+              });
+            }
+          })
+          .catch(err => res.status(400).send({
+            success: false,
+            message: err.message
+          }));
         }
-      } else {
-        res.status(401).json('SignUp to access this service');
-      }
-    })
-    .catch(error => res.status(400).send(error));
+      })
+    } else {
+      return res.status(403).send({
+        success: false,
+        message: 'no token provided'
+      });
+    }
+  },
 
     /*
     groups
@@ -60,7 +89,6 @@ export default {
       .then(res.status(201).send(group));
     })
     .catch(error => res.status(400).send(error));*/
-  },
 
   list(req, res) {
     return Groups
